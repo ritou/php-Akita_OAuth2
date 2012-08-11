b41b4d5f3c5d0d7cd99957c99ebb579b1d4818c4
ritou <ritou.06@gmail.com>
2012-08-11 11:33:52 +0900

    added file for auto-include file

---
+++ src/Akita/OAuth2.php
@@ -0,0 +1,9 @@
+<?php
+
+require_once dirname(__FILE__) . '/OAuth2/Model/AccessToken.php';
+require_once dirname(__FILE__) . '/OAuth2/Model/AuthInfo.php';
+require_once dirname(__FILE__) . '/OAuth2/Server/AuthorizationHandler.php';
+require_once dirname(__FILE__) . '/OAuth2/Server/DataHandler.php';
+require_once dirname(__FILE__) . '/OAuth2/Server/GrantHandlers.php';
+require_once dirname(__FILE__) . '/OAuth2/Server/ProtectedResource.php';
+require_once dirname(__FILE__) . '/OAuth2/Server/Request.php';
